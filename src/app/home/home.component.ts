4f70aea92fb9e286a13d15fd9dc668a661382fbe
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-17 22:24:41 +0100

    Loading Indicator Service - Reactive Implementation Finished

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -34,20 +34,20 @@ export class HomeComponent implements OnInit {
 
   reloadCourses(){
 
-    this.loadingService.loadingOn()
 
     const courses$ = this.coursesService.loadAllCourses()
       .pipe(
         map(courses => courses.sort(sortCoursesBySeqNo)),
-        finalize(() => this.loadingService.loadingOff())
       )
 
-    this.beginnerCourses$ = courses$
+    const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$)
+
+    this.beginnerCourses$ = loadCourses$
       .pipe(
         map(courses => courses.filter(course => course.category == "BEGINNER"))
       )
 
-    this.advancedCourses$ = courses$
+    this.advancedCourses$ = loadCourses$
     .pipe(
       map(courses => courses.filter(course => course.category == "ADVANCED"))
     )
