c97cb48732e23b27bcae1455d21bcfbc37a2cf9c
Samuel-Brr <samuel.barre972@gmail.com>
2022-03-19 10:16:33 +0100

    Local Error Handling in an Angular Material Dialog

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -7,13 +7,15 @@ import {catchError} from 'rxjs/operators';
 import {throwError} from 'rxjs';
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
+import { MessagesService } from '../messages/messages.service';
 
 @Component({
     selector: 'course-dialog',
     templateUrl: './course-dialog.component.html',
     styleUrls: ['./course-dialog.component.css'],
     providers: [
-      LoadingService
+      LoadingService,
+      MessagesService
     ]
 })
 export class CourseDialogComponent implements AfterViewInit {
@@ -26,6 +28,7 @@ export class CourseDialogComponent implements AfterViewInit {
         private coursesService: CoursesService,
         private loadingService: LoadingService,
         private fb: FormBuilder,
+        private messagesService: MessagesService,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
         @Inject(MAT_DIALOG_DATA) course:Course) {
 
@@ -50,6 +53,14 @@ export class CourseDialogComponent implements AfterViewInit {
       const changes = this.form.value;
 
       const saveCourse$ = this.coursesService.saveCourse(this.course.id, changes)
+        .pipe(
+          catchError(err => {
+            const message = "Could not save course"
+            console.log(message, err)
+            this.messagesService.showErrors(message)
+            return throwError(err)
+          })
+        )
 
       this.loadingService.showLoaderUntilCompleted(saveCourse$)
       .subscribe(
